b8168ce4557d08ef1ee32a1f108f445c974e69f2
vijay18399 <vijayreddy18399@gmail.com>
2021-01-01 16:06:12 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+def doubler(string):
+    result=[] 
+    lst = string.split() #split string to list of words
+    for i in lst:
+        if lst.count(i)==2: #checking if particular word count is 2
+            if i not in result: #check if it already stored in result=[]
+                result.append(i) # append working having count exactly 2
+    result.sort()  # sorting result list lexicographically
+    if len(result)==0: #checking if there is no word having count equal to 2
+        return -1
+    else:
+        return result[0] #return the lexically smallest
+string=input()
+print(doubler(string))
